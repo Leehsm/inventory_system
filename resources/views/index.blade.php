e26a245943ea88dc904531f95f6f95b1ff3faa42
Leehsm <shahzleehasimwork@gmail.com>
2023-02-02 10:33:24 +0800

    create index blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,39 @@
+@extends('welcome')
+@section('content')
+@section('title')
+Sahira 
+@endsection
+
+<!-- Slider -->
+<div class="main_slider" style="background-image:url(assets/images/main.png)">
+    <div class="container fill_height">
+        <div class="row align-items-center fill_height">
+            <div class="col">
+                <div class="main_slider_abt" id="aboutus">
+                    <h1>Inventory</h1>
+                    <h6>We help you to change your outfit fashion and be more confident with yourself.</h6>
+                <div class="red_button shop_now_button"><a href="#">shop now</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="main_slider" style="background-image:url(assets/images/main.png)">
+    <div class="container fill_height">
+        <div class="row align-items-center fill_height">
+            <div class="col">
+                <div class="main_slider_abt" id="aboutus">
+                    <h1>Inventory</h1>
+                    <h6>We help you to change your outfit fashion and be more confident with yourself.</h6>
+                <div class="red_button shop_now_button"><a href="#">shop now</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+@endsection
