dd2fba16adcabcb08713f044554e08cd4d4ee418
Leehsm <shahzleehasimwork@gmail.com>
2023-02-02 17:16:16 +0800

    create inventory controller and make CRUD function

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Image;
+
+use App\Models\Product;
 
 class InventoryController extends Controller
 {
@@ -21,6 +24,79 @@ class InventoryController extends Controller
         return view('inventory.add');
     }
 
+    public function inventoryStore(Request $req){
+        $image = $req->file('image');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->save('upload/products/'.$name_gen);
+        $save_url = 'upload/products/'.$name_gen;
+
+        $product = Product::insert([
+            'name' => $req->name,
+            'code' => $req->code,
+            'color' => $req->color,
+            'image' => $save_url,
+            'price' => $req->price,
+        ]);
+
+        return redirect()->route('inventory.add')->with('success', 'Product created successfully!');
+    }
+
+    public function inventoryEdit($id){
+
+        $product = Product::findOrFail($id);
+        return view('inventory.edit', compact('product'));
+    }
+
+    public function inventoryUpdate(Request $req){
+
+        $id = $req->id;
+        $old_image = $req->old_image;
+
+        if($req->file('image')){
+        
+            unlink($old_img);
+            $image = $req->file('image');
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            Image::make($image)->save('upload/products/'.$name_gen);
+            $save_url = 'upload/products/'.$name_gen;
+
+            Product::findOrFail($id)->update([
+                'name' => $req->name,
+                'code' => $req->code,
+                'color' => $req->color,
+                'image' => $save_url,
+                'price' => $req->price,
+            ]);
+
+            $notification = array(
+                'message' => 'Product Updated Successfully',
+                'alert-type' => 'info'
+            );
+
+        }else{
+
+            Product::findOrFail($id)->update([
+                'name' => $req->name,
+                'code' => $req->code,
+                'color' => $req->color,
+                'price' => $req->price,
+            ]);
+
+            $notification = array(
+                'message' => 'Product Updated Successfully',
+                'alert-type' => 'info'
+            );
+
+        }
+        
+        return redirect()->route('inventory')->with($notification);
+
+    }
+
+    public function inventoryDelete(){
+        
+    }
+    
     public function inventorysearch(Request $request){
 
         $query = $request->input('search');
