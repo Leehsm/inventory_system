f1d482144aad7895412935606f31f18e9b50b77b
Leehsm <shahzleehasimwork@gmail.com>
2023-02-10 16:47:55 +0800

    clothing and skincare controller

---
+++ app/Http/Controllers/SkincareController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Skincare;
+
+class SkincareController extends Controller
+{
+    public function skincare(){
+
+        $skincare = Skincare::get();
+
+        return view('skincare.view', compact('skincare'));
+    }
+
+    public function skincareAdd(){
+        return view('skincare.add');
+    }
+
+    public function skincareStore(Request $req){
+
+        $skincare = Skincare::insert([
+            'name' => $req->name,
+            'phone' => $req->phone,
+            'address' => $req->address,
+            'dob' => $req->dob,
+            'admin' => $req->admin,
+        ]);
+
+        return redirect()->route('skincare')->with('success', 'Skincare customer created successfully!');
+
+    }
+
+    
+    public function skincareEdit($id){
+
+        $skincare = Skincare::findOrFail($id);
+        return view('skincare.edit', compact('skincare'));
+
+    }
+
+    public function skincareUpdate(Request $req){
+
+        $id = $req->id;
+
+        Skincare::findOrFail($id)->update([
+            'name' => $req->name,
+            'phone' => $req->phone,
+            'address' => $req->address,
+            'dob' => $req->dob,
+            'admin' => $req->admin,
+        ]);
+
+        $notification = array(
+            'message' => 'Customer Skincare DB Updated Successfully',
+            'alert-type' => 'info'
+        );
+        
+        return redirect()->route('skincare')->with($notification);
+
+    }
+
+    
+    public function skincareDelete($id){
+        
+        Skincare::findOrFail($id)->delete();
+
+        $notification = array(
+            'message' => 'Customer Skincare DB Deleted Successfully',
+            'alert-type' => 'info'
+        );
+
+        return redirect()->back()->with($notification);
+
+    }
+
+    
+    public function skincareSearch(){
+        
+    }
+}
