c0d537359bd554a4c1711580ac97cdfd38e38969
Leehsm <shahzleehasimwork@gmail.com>
2023-02-15 09:11:39 +0800

    edit index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,33 +4,42 @@
 Sahira 
 @endsection
 
-<!-- Slider -->
-<div class="main_slider" style="background-image:url(assets/images/main.png)">
-    <div class="container fill_height">
-        <div class="row align-items-center fill_height">
-            <div class="col">
-                <div class="main_slider_abt" id="aboutus">
-                    <h1>Inventory</h1>
-                    <h6>We help you to change your outfit fashion and be more confident with yourself.</h6>
-                <div class="red_button shop_now_button"><a href="#">shop now</a></div>
-                </div>
-            </div>
+<div class="benefit">
+    <div class="col text-center" id="blog">
+        <div class="section_title">
+        <h2>Order</h2>
         </div>
     </div>
+    <br>
 </div>
 
-<div class="main_slider" style="background-image:url(assets/images/main.png)">
-    <div class="container fill_height">
-        <div class="row align-items-center fill_height">
-            <div class="col">
-                <div class="main_slider_abt" id="aboutus">
-                    <h1>Inventory</h1>
-                    <h6>We help you to change your outfit fashion and be more confident with yourself.</h6>
-                <div class="red_button shop_now_button"><a href="#">shop now</a></div>
-                </div>
-            </div>
+<div class="container">
+    <form action="{{ route('inventory.search') }}" method="GET" class="form-inline">
+        <div class="form-group mx-sm-3 mb-2">
+          <input type="text" class="form-control" id="search" placeholder="Name / Code">
+        </div>
+        <button type="submit" class="btn btn-primary mb-2">Search</button>
+    </form>
+    <div class="row">
+    @foreach ($product as $data)
+      <div class="col-sm-6 col-lg-4 mb-3">
+        <div class="card h-100">
+          <img src="{{ asset($data->image) }}" class="card-img-top" style="width: 250px; height: 250px;">
+          <div class="card-body">
+            <input type="hidden" name="name" value="{{ $data->name }}">
+            <input type="hidden" name="code" value="{{ $data->code }}">
+            <input type="hidden" name="color" value="{{ $data->color }}">
+            <input type="hidden" name="price" value="{{ $data->price }}">
+            <h5 class="card-title">{{ $data->name }}</h5>
+            <p class="card-text">{{ $data->code }}</p>
+            <p class="card-text">{{ $data->color }}</p>
+            <p class="card-text">{{ $data->price }}</p>
+            <a href="#" class="btn btn-primary">ADD</a>
+          </div>
         </div>
     </div>
+    @endforeach
+  </div>
 </div>
 
 
