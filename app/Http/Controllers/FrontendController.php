7bdb2acacd71e02cb37c38ba54a10f04799fa991
Leehsm <shahzleehasimwork@gmail.com>
2023-02-15 09:13:10 +0800

    altered index function

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Image;
+
+use App\Models\Product;
 
 
 class FrontendController extends Controller
 {
     public function index(){
-        return view('index');
+
+        $product = Product::get();
+        return view('index', compact('product'));
     }
 }
