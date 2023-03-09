59bec34b45234980e7a59c6a493bd0d1cb51f977
Leehsm <shahzleehasimwork@gmail.com>
2023-03-09 17:27:21 +0800

    make model fillable

--- app/Models/Skincare.php
+++ app/Models/Skincare.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Skincare extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'phone',
+        'address',
+        'dob',
+        'admin',
+    ];
 }
